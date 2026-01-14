0159d92a8fd7a03eaffd210495dfb76916f1edd5
nikitkadev <nekitosik.pav@gmail.com>
2026-01-14 16:50:38 +0700

    feature: add voice channel time metric for server members

---
+++ 2_Application/Events/UserVoiceStateUpdated/VoiceSessionTimeHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MlkAdmin._1_Domain.Entities;
+using MlkAdmin._1_Domain.Interfaces;
+using MlkAdmin._2_Application.Interfaces.Managers;
+using MlkAdmin._2_Application.Interfaces.Services;
+
+namespace MlkAdmin._2_Application.Events.UserVoiceStateUpdated;
+
+public class VoiceSessionTimeHandler(
+    IGuildVoiceSessionsManager voiceSessionsManager) : INotificationHandler<UserVoiceStateUpdated>
+{
+    public async Task Handle(UserVoiceStateUpdated notification, CancellationToken cancellationToken)
+    {
+        await voiceSessionsManager.HandleVoiceStateUpdateAsync(
+            notification.NewState.VoiceChannel, 
+            notification.OldState.VoiceChannel, 
+            notification.SocketUser.Id);
+    }
+}
---
+++ 2_Application/Interfaces/Services/IVoiceSessionCacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace MlkAdmin._2_Application.Interfaces.Services;
+
+public interface IVoiceSessionCacheService
+{
+    void SetVoiceSessionStart(ulong guildMemberDiscordId);
+    DateTimeOffset? GetVoiceSessionStartByMemberDiscordId(ulong guildMemberDiscordId);
+    void RemoveVoiceSessionFromDictionary(ulong guildMemberDiscordId);
+    bool AlreadySet(ulong guildMemberDiscordId);
+}
