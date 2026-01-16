28c7119e00084b459ea3f85faa29a94c20640b80
nikitkadev <nekitosik.pav@gmail.com>
2026-01-16 11:07:35 +0700

    feature: add SendAnalyzeResultMessageAsync method for sending analyse result

--- Shared/Dtos/GuildMemberAnalyzeData.cs
+++ Shared/Dtos/GuildMemberAnalyzeData.cs
@@ -2,19 +2,24 @@
 
 public record GuildMemberAnalyzeData
 {
+    public ulong GuildMemberDiscordId { get; set; }
+
+    public DateTime JoinedAt { get; init; }
+    public DateTime FirstMessageDate { get; init; }
+    public DateTime LastMessageDate { get; init; }
+
     public int MessageCount { get; init; }
     public int ReactionCount { get; init; }
     public int PicturesSentCount { get; init; }
     public int GifsSentCount { get; init; }
     public int CommandsSentCount { get; init; }
-    public int EmotesSentCount { get; init; }
     public long VoiceChannelsTimeSpent { get; init; }
-
     public int DaysSinceJoined { get; init; }
-    public float Toxicity {  get; init; }
 
-    public DateTime JoinedAt { get; init; }
-    public DateTime FirstMessageDate { get; init; }
-    public DateTime LastMessageDate { get; init; }
+    public float AvgToxicity { get; init; }
+    public string MostToxicMessage { get; init; } = string.Empty;
+    public string SpeechStyle { get; init; } = string.Empty;
+    public string Tonality { get; init; } = string.Empty;
+    public float AvgCharsInMessage { get; init; }
 
 }
